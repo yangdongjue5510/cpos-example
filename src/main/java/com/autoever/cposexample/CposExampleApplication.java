8081cb9c16cf71fab906f6d9238b9abb0db5d967
1128016 <djwhy5510@hyundai-autoever.com>
2024-05-10 14:16:19 +0900

    project init

---
+++ src/main/java/com/autoever/cposexample/CposExampleApplication.java
@@ -0,0 +1,13 @@
+package com.autoever.cposexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CposExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CposExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autoever/cposexample/CposExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoever.cposexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CposExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
